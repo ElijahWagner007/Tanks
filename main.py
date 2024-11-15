35437a9585d24d4688eff0e217d8c8937e2ca80c
ElijahWagner007 <elijahnwagner@gmail.com>
2024-11-15 18:40:38 -0500

    "First Commit"

---
+++ main.py
@@ -0,0 +1,50 @@
+import pygame as pg
+from pygame.locals import *
+import math as m
+
+from ufo import Ufo
+from game import SCREEN_WIDTH , SCREEN_HEIGHT
+
+# Initialize pygame
+pg.init()
+
+# Create Window, set window caption, start clock
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pg.display.set_caption("Ufos?!")
+clock = pg.time.Clock()
+speed_limit = pg.Vector2(1.5,1.5)
+
+# Colors
+bg = (247, 210, 166)
+bullet_color = (128,140,140)
+
+# Game Vars.
+p1_magazine = []
+
+p1_ufo = Ufo(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, screen, "PLAYER")
+enemy_ufo = Ufo(SCREEN_WIDTH // 4, SCREEN_HEIGHT // 4, screen, "ENEMY")
+ctr = 59
+
+# Game Loop
+running = True
+while running:
+    # set clock / delta time in seconds since last frame
+    # i.e. framerate independant physics
+    dt = clock.tick(60) / 1000
+    ctr += 1       
+    
+    screen.fill(bg)
+    # QUIT Event
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    p1_ufo.draw_player(dt)
+    enemy_ufo.draw_cpu(dt, ctr)
+
+
+    # Updates display
+    pg.display.flip() 
+    
+# De-initializes pygame
+pg.quit() 
---
+++ ufo.py
@@ -0,0 +1,117 @@
+import pygame as pg
+import random as r
+from game import SCREEN_WIDTH , SCREEN_HEIGHT
+
+
+class Ufo():
+    """
+        ufo class x and y are center of ufo
+    """
+    def __init__(self, x, y, screen, ufo_type) -> None:
+        self.x = x
+        self.y = y
+        self.ufo = pg.transform.scale(pg.image.load("./assets/"+ ufo_type +".png").convert_alpha(), (60,60))
+        self.speed = 5
+        self.screen = screen
+
+        # Kinematic Movement stuffs
+        self.ufo_pos = pg.Vector2(x,y)
+        self.ufo_vel = pg.Vector2(0,0) # Don't move at start
+        self.ufo_accel = pg.Vector2(0,0) # Don't speed up at start
+
+        self.ACCEL = 20 
+    def screen_collision(self):
+        half_width = (self.ufo.get_width() // 2)
+        half_height = (self.ufo.get_height() // 2)
+
+        # Check for collisions with walls
+        if self.ufo_pos.x + half_width > SCREEN_WIDTH:
+            self.ufo_pos.x = SCREEN_WIDTH - half_width
+            self.ufo_vel.x *= -0.2
+        if self.ufo_pos.x - half_width < 0:
+            self.ufo_pos.x = half_width
+            self.ufo_vel.x *= -0.2
+        if self.ufo_pos.y + half_height > SCREEN_HEIGHT:
+            self.ufo_pos.y = SCREEN_HEIGHT - half_height
+            self.ufo_vel.y *= -0.2 
+        if self.ufo_pos.y - half_height < 0:
+            self.ufo_pos.y = half_height 
+            self.ufo_vel.y *= -0.2 
+
+    def cpu_screen_collision(self):
+        half_width = (self.ufo.get_width() // 2)
+        half_height = (self.ufo.get_height() // 2)
+
+        # Check for collisions with walls
+        if self.ufo_pos.x + half_width > SCREEN_WIDTH:
+            self.ufo_pos.x = SCREEN_WIDTH - half_width
+            self.ufo_vel.x *= -0.2
+            self.ufo_accel *= -0.2
+        if self.ufo_pos.x - half_width < 0:
+            self.ufo_pos.x = half_width
+            self.ufo_vel.x *= -0.2
+            self.ufo_accel *= -0.2
+        if self.ufo_pos.y + half_height > SCREEN_HEIGHT:
+            self.ufo_pos.y = SCREEN_HEIGHT - half_height
+            self.ufo_vel.y *= -0.2 
+            self.ufo_accel *= -0.2
+        if self.ufo_pos.y - half_height < 0:
+            self.ufo_pos.y = half_height 
+            self.ufo_vel.y *= -0.2 
+            self.ufo_accel *= -0.2
+
+
+    def update(self,dt):
+        # set initial accel to 0,0
+        self.ufo_accel = pg.Vector2(0,0)
+        
+        # Check for movement updates
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            self.ufo_accel.y = -self.ACCEL * dt
+        if keys[pg.K_s]:
+            self.ufo_accel.y = self.ACCEL * dt
+        if keys[pg.K_d]:
+            self.ufo_accel.x = self.ACCEL * dt
+        if keys[pg.K_a]:
+            self.ufo_accel.x = -self.ACCEL * dt
+        
+        self.ufo_vel += self.ufo_accel
+        self.screen_collision()        
+        self.ufo_pos += self.ufo_vel + 0.5 * self.ufo_accel
+
+    def draw_player(self, dt):
+        """
+            Draws ufo onto window
+        """
+        self.update(dt)
+        self.screen.blit(self.ufo,(self.ufo_pos.x - self.ufo.get_width() // 2, self.ufo_pos.y - self.ufo.get_width() // 2))
+    
+    def update_cpu(self,dt,ctr):
+        if ctr % 60 == 0:
+            # set initial accel to 0,0
+            self.ufo_accel = pg.Vector2(0,0)
+            
+            # Check for movement updates
+            rand_dir = r.randint(0,3)
+            if rand_dir == 0:
+                self.ufo_accel.y = -10 * self.ACCEL * dt 
+            if rand_dir == 1:
+                self.ufo_accel.y = 10 * self.ACCEL * dt
+            if rand_dir == 2:
+                self.ufo_accel.x = 10 * self.ACCEL * dt
+            if rand_dir == 3:
+                self.ufo_accel.x = -10 * self.ACCEL * dt
+            
+            self.ufo_vel += self.ufo_accel
+            
+        self.cpu_screen_collision()
+        self.ufo_pos += self.ufo_vel + 0.5 * self.ufo_accel
+
+
+    def draw_cpu(self, dt, ctr):
+        """
+            Draws ufo onto window. Distances not hardcoded, that's just the center of the ufo
+        """
+        self.update_cpu(dt, ctr)
+        self.screen.blit(self.ufo,(self.ufo_pos.x - self.ufo.get_width() // 2, self.ufo_pos.y - self.ufo.get_width() // 2))
